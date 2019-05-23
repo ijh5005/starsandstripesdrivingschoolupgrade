6d1f47c245d9097b9f6284b784f2b3446383ec97
Isaiah Harrison <isaiahharrison@Isaiahs-MacBook-Pro-2.local>
2019-05-22 21:04:20 -0400

    class to className change

--- src/Contactpage/ContactForm/index.js
+++ src/Contactpage/ContactForm/index.js
@@ -47,18 +47,18 @@ const ContactForm = () => {
 
   return (<div id="contactForm" className="flexCol">
     <p className="smallText contactHeading">Contact Us</p>
-    <div class="form-style-2">
+    <div className="form-style-2">
       <form action="" method="post">
         <label for="field1">
           <span>Name</span>
-          <input type="text" class="input-field" name="field1" value={name} onChange={nameChange.bind(this)}/>
+          <input type="text" className="input-field" name="field1" value={name} onChange={nameChange.bind(this)}/>
         </label>
         <label for="field2">
           <span>Email</span>
-          <input type="text" class="input-field" name="field2" value={contact} onChange={contactChange.bind(this)}/>
+          <input type="text" className="input-field" name="field2" value={contact} onChange={contactChange.bind(this)}/>
         </label>
         <label for="field5"><span>Message</span>
-          <textarea name="field5" class="textarea-field" value={message} onChange={messageChange.bind(this)}></textarea></label>
+          <textarea name="field5" className="textarea-field" value={message} onChange={messageChange.bind(this)}></textarea></label>
         <label>
         <span> </span><input type="submit" value="Submit" onClick={submitEmail.bind(this)} /></label>
       </form>
