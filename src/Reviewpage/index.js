3e766609d5b5711d80cdd8418f2ed05f9ee81c40
Isaiah Harrison <isaiahharrison@Isaiahs-MacBook-Pro-2.local>
2019-05-28 19:18:39 -0400

    review button added

--- src/Reviewpage/index.js
+++ src/Reviewpage/index.js
@@ -11,7 +11,7 @@ import avatar5 from './ppl4.png'
 function Reviewpage() {
 
   window.scrollTo(0, 0);
-  
+
   return (<div id="servicepage" className="flexCol">
     <p className="title servicepageTitle">
       <img className="starsBanner" src={stars} />
@@ -24,6 +24,10 @@ function Reviewpage() {
     </p>
     <Testimonial img={avatar5} img2={avatar3}/>
     <Testimonial img={avatar} img2={avatar4}/>
+    <a className="aBanner" href="https://www.google.com/search?q=stars+and+stripes+driving&rlz=1C5CHFA_enUS813US813&oq=star&aqs=chrome.0.69i59j69i60l2j69i57j69i60l2.902j0j7&sourceid=chrome&ie=UTF-8#lrd=0x89c6c795c329ec55:0x2f3dd6f67ada709,3,,," target="_blank">
+      <img className="starsBanner" src={stars} />
+      <p className="title servicepageTitle">Click to make a review</p>
+    </a>
   </div>);
 }
 
