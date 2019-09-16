187ec3423c70139dc9b70ad84a0dc5a4c42960fa
ijh5005 <ijh5005@outlook.com>
2019-09-16 18:38:58 -0400

    Update other_pricing.js

--- src/Servicepage/pricing/other_pricing.js
+++ src/Servicepage/pricing/other_pricing.js
@@ -3,7 +3,7 @@ const other_pricing = [
     cost: "$80",
     text: [
       "Car Rental",
-      "FREE 15 minutes parallel parking practice"
+      //"FREE 15 minutes parallel parking practice"
     ]
   },
   {
@@ -11,7 +11,7 @@ const other_pricing = [
     text: [
       "Car Rental",
       "30 minutes practice",
-      "FREE 15 minutes parallel parking practice"
+      //"FREE 15 minutes parallel parking practice"
     ]
   },
   {
@@ -19,7 +19,7 @@ const other_pricing = [
     text: [
       "Car Rental",
       "1 hour practice",
-      "FREE 15 minutes parallel parking practice"
+      //"FREE 15 minutes parallel parking practice"
     ]
   }
 ];
