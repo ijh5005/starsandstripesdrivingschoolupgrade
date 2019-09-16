7cdf9d8efbcc6679b9fccbcb441bbdfc222ae638
ijh5005 <ijh5005@outlook.com>
2019-09-16 18:29:20 -0400

    Update driving_test_prices.js

--- src/Servicepage/pricing/driving_test_prices.js
+++ src/Servicepage/pricing/driving_test_prices.js
@@ -1,10 +1,10 @@
 const driving_test_prices = [
   {
-    text: ["1 hr driving lesson in south philly"],
+    text: ["1 hr driving lesson in south philly (minimum 2 hrs)"],
     cost: "$35"
   },
   {
-    text: ["1 hr driving lesson outside south philly"],
+    text: ["1 hr driving lesson outside south philly (minimum 2 hrs)"],
     cost: "$45"
   }
 ];
